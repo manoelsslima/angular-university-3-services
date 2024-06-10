b32c590aa8624cc78dd86aa1d0ba408522ea0ac4
Manoel Lima <manoel.lima@manaus.am.gov.br>
2024-06-10 13:53:43 -0400

    Angular Custom Service - Data Modification with an HTTP PUT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { CoursesService } from './services/courses.service';
 
 @Component({
@@ -21,6 +21,13 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.listCourses$ = this.coursesService.loadCourses();
+  }
+
+  save(course: Course) {
+    this.coursesService.saveCourse(course)
+      .subscribe(
+        () => console.log('Course saved!')
+      );
   }
 
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,27 +1,30 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Course } from '../model/course';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Course } from "../model/course";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CoursesService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   /**
    * Retorna um Observable de array de Course
-   * 
+   *
    * @returns course list
    */
   loadCourses(): Observable<Course[]> {
+    const params = new HttpParams().set("page", "1").set("pageSize", "10");
 
-    const params = new HttpParams()
-    .set("page", "1")
-    .set("pageSize", "10");
-
-    return this.http.get<Course[]>('/api/courses', { params: params });
+    return this.http.get<Course[]>("/api/courses", { params: params });
   }
 
+  saveCourse(course: Course) {
+    
+    const headers = new HttpHeaders()
+      .set("X-Auth", "userId");
+
+    return this.http.put(`/api/courses/${course.id}`, course, { headers: headers });
+  }
 }
