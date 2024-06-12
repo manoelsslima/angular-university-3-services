8123883676d4907bb33f37ab34e503608de4cb4f
Manoel Lima <manoel.lima@manaus.am.gov.br>
2024-06-12 14:24:12 -0400

    Angular DI - Injection Tokens in Detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, Inject, InjectionToken, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Inject, InjectionToken, OnInit, QueryList, ViewChild, ViewChildren, inject} from '@angular/core';
 import {COURSES} from '../db-data';
 import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
@@ -6,18 +6,22 @@ import {HighlightedDirective} from './directives/highlighted.directive';
 import {Observable} from 'rxjs';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { CoursesService } from './services/courses.service';
+import { APP_CONFIG, AppConfig, CONFIG_TOKEN} from './config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [ CoursesService ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
   listCourses$: Observable<Course[]>;
 
-  constructor(private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService,
+    @Inject(CONFIG_TOKEN) private config: AppConfig) {
+
+    console.log(this.config);
   }
 
   ngOnInit() {
