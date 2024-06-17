9c314d240d703ac71ee1e24a26683f09473809e0
Manoel Lima <manoelsslima@yahoo.com.br>
2024-06-16 23:26:24 -0400

    Angular Custom Change Detection with ChangeDetectorRef

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, Inject, OnInit } from '@angular/core';
 import { Course } from './model/course';
 import { Observable } from 'rxjs';
 import { CoursesService } from './services/courses.service';
@@ -7,20 +7,35 @@ import { AppConfig, CONFIG_TOKEN } from './config';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, DoCheck {
 
-  courses$: Observable<Course[]>;
+  //courses$: Observable<Course[]>;
+  courses: Course[];
+  loaded = false;
 
   constructor(
     private coursesService: CoursesService,
-    @Inject(CONFIG_TOKEN) private config: AppConfig) {
-    console.log(this.config);
+    @Inject(CONFIG_TOKEN) private config: AppConfig,
+    private cd: ChangeDetectorRef) {
+  }
+
+  ngDoCheck() {
+    console.log("ngDoCheck()");
+    if (this.loaded) {
+      this.cd.markForCheck();
+      console.log("called cd.markForCheck()");
+      this.loaded = undefined;
+    }
   }
 
   ngOnInit() {
-    this.courses$ = this.coursesService.loadCourses();
+    //this.courses$ = this.coursesService.loadCourses();
+    this.coursesService.loadCourses().subscribe(list => this.courses = list);
+    this.loaded = true;
+    console.log("ngOnInit()");
   }
 
   save(course: Course) {
